578976cc189afedc6803a7c987a4d03383f09ff3
ourbluesky <791518170@qq.com>
2017-07-05 12:38:59 +0800

    progress bar

--- DSIES/Pages/LoginPage.xaml.cs
+++ DSIES/Pages/LoginPage.xaml.cs
@@ -81,7 +81,7 @@ namespace DSIES.Pages
         {
             Read_Register_Data();
             PageList.SceneSelect = new SceneSelectPage();
-            PageList.Main.setPage(PageList.SceneSelect);//Questionandgame);//
+            PageList.Main.setPage(PageList.Questionandgame);//Questionandgame);//.SceneSelect
         }
     }
 }
